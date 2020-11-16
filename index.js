9136961bba916e80e647d8b76be4a00854363357
Oliver <olliepar01@gmail.com>
2020-11-16 12:23:50 +0000

    Added a reactjs frontend

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the music player');
-})
+app.use(express.static('build'));
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: `/build/` });
+});
 
 app.listen(port, () => console.log('App listening on port ' + port));
